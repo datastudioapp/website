f2f9859db0311b794d7ca58adc65a08a1793c41e
echus <varvara@echus.co>
2024-08-28 11:15:31 +1000

    opendata.studio updates, placeholders for homepage

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "opendata.fit";
-export const SITE_DESCRIPTION = "An open science platform";
+export const SITE_TITLE = "opendata.studio";
+export const SITE_DESCRIPTION = "Software for open science and data analysis";
