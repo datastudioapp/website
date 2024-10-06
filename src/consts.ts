958ef1caf708accca485eee8aaed5067415619b0
Varvara Efremova <varvara@echus.co>
2024-10-06 12:39:04 +1100

    Remove unused blog pages and components

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = "opendata.studio";
-export const SITE_DESCRIPTION = "Software for open science and data analysis";
+export const SITE_DESCRIPTION = "Reproducible data publication for the web";
