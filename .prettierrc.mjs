e40c3fb4fd9985fa1a6e5a2c277bda5199a4aed6
Varvara Efremova <varvara@echus.co>
2025-07-11 16:07:03 +1000

    Initial Starter Cosmic Theme template

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,19 +1,36 @@
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ["prettier-plugin-astro"],
-  astroAllowShorthand: false,
-  proseWrap: "always",
+  printWidth: 100,
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  useTabs: true,
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
   overrides: [
     {
-      files: "*.astro",
+      files: [".*", "*.md", "*.toml", "*.yml"],
       options: {
-        parser: "astro",
+        useTabs: false,
       },
     },
     {
-      files: "*.json",
+      files: ["**/*.mdx"],
       options: {
-        trailingComma: "none",
+        /**
+         * feel free to change this. Keystatic expects a certain format for JSX components in MDX files
+         * and me setting this to 80 forces my demo files to work correctly
+         * I suggest EITHER using keystatic for all blog posts, OR using MD/MDX for all blog posts
+         *
+         * IF using keystatic, I recommend uncommenting the MDX formatting line in .prettierignore
+         */
+        printWidth: 80,
+      },
+    },
+    {
+      files: ["**/*.astro"],
+      options: {
+        parser: "astro",
       },
     },
   ],
--- src/consts.ts
+++
@@ -1,2 +0,0 @@
-export const SITE_TITLE = "The opendata.studio project";
-export const SITE_DESCRIPTION = "Declarative data analysis for the web";
