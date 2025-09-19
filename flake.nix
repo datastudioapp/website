ede6a96a3ed31195a30cffd9181d70d5690169c5
James Wilmot <jameswilmot2000@gmail.com>
2025-09-19 13:44:23 +1000

    Converted from yarn to npm; added wrangler; flake.nix tweaks

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "opendata.studio web-components development flake";
+  description = "opendata.fit development environment for the webapp and frontend packages";
 
   # Use the unstable nixpkgs to use the latest set of node packages
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
@@ -19,10 +19,19 @@
         buildInputs = [
           # Set the major version of Node.js
           pkgs.nodejs_22
-          pkgs.yarn-berry
           pkgs.nodePackages.typescript
           # pkgs.nodePackages.typescript-language-server
+
+          # dependencies for grip markdown viewer
         ];
+
+        # See: https://github.com/cloudflare/workerd/issues/1482
+          shellHook = ''
+            __patchTarget="./node_modules/@cloudflare/workerd-linux-64/bin/workerd"
+            if [[ -f "$__patchTarget" ]]; then
+              ${pkgs.patchelf}/bin/patchelf --set-interpreter ${pkgs.glibc}/lib/ld-linux-x86-64.so.2 "$__patchTarget"
+            fi
+          '';
       };
     });
 }
